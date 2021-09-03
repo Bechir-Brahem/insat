dd6285deec2d76b5163b4ca3aefb46f50cebd76e
Bechir-Brahem <bachbrahem@gmail.com>
2021-09-03 23:25:50 +0100

    initial after wireframe

--- assets/main.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('#carouselExampleCaptions').on('slide.bs.carousel', function (e) {
-        // console.log(e);
-        $('.news-list button').removeClass('active');
-        $('.news-list button[data-bs-slide-to='+e.to+']').addClass('active');
-    })
-})  
