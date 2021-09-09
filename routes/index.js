c38e634ecb8fa6783d5b0bdfed61abb041775740
Bechir-Brahem <bachbrahem@gmail.com>
2021-09-10 00:51:12 +0100

    added actutalite pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-	res.render('index', { title: 'Express' });
+	res.render('index/index', { title: 'Express' });
+});
+router.get('/actualite', function (req, res) {
+	res.render('actualite/actualites', { title: 'Express' });
+});
+router.get('/actualite/1', function (req, res) {
+	res.render('actualite/actualite', { title: 'Express' });
 });
 
 module.exports = router;
